a079509629109dd1327e2110458e1779850640e0
shalluv <leon555thai@gmail.com>
2025-01-23 12:37:12 +0700

    feat: hospitals api

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+const hospitals = require("./routes/hospitals");
+
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ success: true, data: { id: 1 } });
-});
+app.use("/api/v1/hospitals", hospitals);
 
 const PORT = process.env.PORT || 5000;
 
